d8fb082cc268381e41967964bb5394d920b46cc3
Voland91 <you@example.com>
2021-04-21 22:09:16 +0200

    added styles for headers

--- ui/src/views/Root.tsx
+++ ui/src/views/Root.tsx
@@ -1,7 +1,7 @@
 import GlobalStyle from '../theme/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import theme from '../theme/mainTheme';
-import Header from '../components/atoms/Header/Header';
+import Headers from '../components/atoms/Headers/Headers';
 import Text from '../components/atoms/Text/Text';
 import SearchBar from '../components/molecules/SearchBar/SearchBar';
 
@@ -9,7 +9,9 @@ const Root: React.FC = () => (
   <>
     <GlobalStyle />
     <ThemeProvider theme={theme}>
-      <Header>header</Header>
+      <Headers type="h1">header1</Headers>
+      <Headers type="h2">header2</Headers>
+      <Headers type="h3">header3</Headers>
       <Text>server name</Text>
       <SearchBar />
     </ThemeProvider>
