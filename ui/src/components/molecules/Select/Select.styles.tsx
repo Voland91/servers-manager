cb603badabbe98185e8116950d9994ec31752e42
Voland91 <you@example.com>
2021-04-24 13:10:41 +0200

    added some fixes

---
+++ ui/src/components/molecules/Select/Select.styles.tsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+const SelectStyles = styled.div``;
+
+export default SelectStyles;
---
+++ ui/src/components/molecules/Select/Select.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+interface SelectionProps {
+  status: string;
+}
+
+const Select: FC<SelectionProps> = ({ status }) => <></>;
+
+export default Select;
